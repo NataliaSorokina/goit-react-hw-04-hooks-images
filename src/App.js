28794d8a91d7170cd12e7c1234308f41cedb8c54
Natalia Sorokina <pticasonabird@gmail.com>
2021-09-29 16:26:23 +0300

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { AppComponent } from './App.styled.js';
@@ -9,54 +9,49 @@ import Button from 'components/Button/Button';
 import Loader from 'components/Loader/Loader';
 import Modal from 'components/Modal/Modal';
 
-class App extends React.Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    per_page: 12,
-    totalImages: 0,
-    images: [],
-    searchStatus: 'idle',
-    largeImage: '',
-    showModal: false,
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [per_page /* , setPer_page */] = useState(12);
+  const [totalImages, setTotalImages] = useState(0);
+  const [images, setImages] = useState([]);
+  const [searchStatus, setSearchStatus] = useState('idle');
+  const [largeImage, setLargeImage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+    setPage(1);
+    setImages([]);
   };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({
-      searchQuery: searchQuery,
-      page: 1,
-      images: [],
-    });
+  const handlePageIncrement = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  handlePageIncrement = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleModalToggle = () => {
+    setShowModal(!showModal);
   };
 
-  handleModalToggle = fullImage => {
-    this.setState(({ showModal }) => ({
-      largeImage: !showModal ? fullImage : '',
-      showModal: !showModal,
-    }));
+  const handleLargeImage = fullImage => {
+    setLargeImage(fullImage);
+    handleModalToggle();
   };
 
-  handleScroll = () => {
+  const handleScroll = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
   };
 
-  async componentDidUpdate(_, prevState) {
-    const { searchQuery, page, per_page } = this.state;
-    const shouldFetch =
-      prevState.searchQuery !== searchQuery || prevState.page !== page;
-
-    if (shouldFetch) {
+  useEffect(() => {
+    async function getImages() {
       try {
-        this.setState({ searchStatus: 'pending' });
+        if (searchQuery === '') {
+          return;
+        }
+        setSearchStatus('pending');
         const result = await fetchImages(searchQuery, page, per_page);
         const { hits, totalHits } = result;
         if (hits.length === 0) {
@@ -71,50 +66,35 @@ class App extends React.Component {
         if (totalHits < page * per_page) {
           toast('Sorry, but you have reached the end of search results.');
         }
-        this.setState(
-          prevState => ({
-            searchStatus: 'resolved',
-            images: [...prevState.images, ...hits],
-            totalImages: totalHits,
-          }),
-          () => {
-            this.handleScroll();
-          },
-        );
+
+        setSearchStatus('resolved');
+        setImages(prevState => [...prevState, ...hits]);
+        setTotalImages(totalHits);
+        handleScroll();
       } catch (error) {
-        this.setState({ searchStatus: 'rejected' });
+        setSearchStatus('rejected');
         toast.error('Error. We are sorry, but something went wrong.');
         console.log(error);
       }
     }
-  }
+    getImages();
+  }, [searchQuery, page, per_page]);
 
-  render() {
-    const {
-      images,
-      searchStatus,
-      per_page,
-      totalImages,
-      showModal,
-      largeImage,
-    } = this.state;
-    const shouldRenderButton =
-      totalImages > per_page && searchStatus !== 'pending';
-    return (
-      <AppComponent>
-        <Searchbar onFormSubmit={this.handleFormSubmit} />
-        {searchStatus === 'pending' && <Loader />}
-        {images.length > 0 && (
-          <ImageGallery images={images} onSelect={this.handleModalToggle} />
-        )}
-        {shouldRenderButton && <Button onClick={this.handlePageIncrement} />}
-        {showModal && (
-          <Modal fullImage={largeImage} onClose={this.handleModalToggle} />
-        )}
-        <ToastContainer autoClose={3000} />
-      </AppComponent>
-    );
-  }
+  const shouldRenderButton =
+    totalImages > per_page && searchStatus !== 'pending';
+
+  return (
+    <AppComponent>
+      <Searchbar onFormSubmit={handleFormSubmit} />
+      {searchStatus === 'pending' && <Loader />}
+      {images.length > 0 && (
+        <ImageGallery images={images} onSelect={handleLargeImage} />
+      )}
+      {shouldRenderButton && <Button onClick={handlePageIncrement} />}
+      {showModal && (
+        <Modal fullImage={largeImage} onClose={handleModalToggle} />
+      )}
+      <ToastContainer autoClose={3000} />
+    </AppComponent>
+  );
 }
-
-export default App;
